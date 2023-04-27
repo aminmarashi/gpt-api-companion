f174773cdc051e4f1b2151d03e895c2436927e39
Amin Marashi <me@amin.codes>
2023-04-27 09:06:54 +0200

    fix: make the spinner spin again

--- chat/pages/index.tsx
+++ chat/pages/index.tsx
@@ -382,47 +382,6 @@ export default function Home() {
             <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2 mt-2">
               <span ref={spinnerRef} id="spinner" className="mr-1 hidden">
                 <svg className="inline w-4 h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <style>
-                    {
-
-                      `.spinner_jCIR {
-                    animation: spinner_B8Vq .9s linear infinite;
-                    animation- delay: -.9s
-                  }
-
-                      .spinner_upm8 {
-                  animation - delay: -.8s
-                }
-
-                      .spinner_2eL5 {
-                  animation - delay: -.7s
-                }
-
-                      .spinner_Rp9l {
-                  animation - delay: -.6s
-                }
-
-                      .spinner_dy3W {
-                  animation - delay: -.5s
-                }
-
-                @keyframes spinner_B8Vq {
-
-                  0 %,
-                    66.66 % {
-                      animation- timing - function: cubic- bezier(0.36, .61, .3, .98);
-                  y: 6px;
-                  height: 12px
-                }
-
-                33.33 % {
-                  animation - timing - function: cubic- bezier(0.36, .61, .3, .98);
-                y: 1px;
-                height: 22px
-              }
-            }
-            `}
-                  </style>
                   <rect className="spinner_jCIR" x="1" y="6" fill="white" width="2.8" height="12" />
                   <rect className="spinner_jCIR spinner_upm8" fill="white" x="5.8" y="6" width="2.8" height="12" />
                   <rect className="spinner_jCIR spinner_2eL5" fill="white" x="10.6" y="6" width="2.8" height="12" />
